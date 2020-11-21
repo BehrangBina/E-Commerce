b27bc26ad4ac35bf09760e6b5bab631fa7cf348a
BehrangBina <BehrangBina@hotmail.com>
2020-11-21 19:34:31 +1100

    add prodict to db

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,21 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("/api/[controller]")]
-    public class ProductsController:ControllerBase
+    public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
         //  https://localhost:5001/api/products
         [HttpGet]
-        public string GetProducts(){
-            return "All products will be returned";
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products= await _storeContext.products.ToListAsync();
+            return Ok(products);
         }
 
         //  https://localhost:5001/api/products/12
         [HttpGet("{iD}")]
-        public string GetProduct(int Id){
-            return "Product returned: "+Id;
+        public async Task<ActionResult<Product>>  GetProduct(int Id)
+        {
+            return await _storeContext.products.FindAsync(Id);
         }
     }
 }
