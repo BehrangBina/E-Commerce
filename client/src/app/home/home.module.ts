901736fec96391b107df365dbf1d98a69a8e64ba
BehrangBina <BehrangBina@hotmail.com>
2021-02-10 22:05:05 +1100

    Add Different Modules

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShopModule } from '../shop/shop.module';
 import { HomeComponent } from './home.component';
-
-
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HomeComponent]
 })
 export class HomeModule { }
