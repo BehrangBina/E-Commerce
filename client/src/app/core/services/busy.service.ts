b2bfab8cd4428efa72858adebd25a3138debc590
BehrangBina <BehrangBina@hotmail.com>
2021-04-15 20:46:44 +1000

    Add loading icon

---
+++ client/src/app/core/services/busy.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  buisyRequestCount=0;
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy(){
+    this.buisyRequestCount++;
+    this.spinnerService.show(undefined,{
+      type: 'ball-climbing-dot',
+      bdColor: 'rgba(255,255,255,0,0.7)',
+      color:'#333333'
+    })
+  }
+
+  idle(){
+    this.buisyRequestCount--;
+    if(this.buisyRequestCount<=0){
+      this.buisyRequestCount=0;
+      this.spinnerService.hide();
+    }
+  }
+}
